75432386e5adfc5e10134ae4d4e8711f5cedf387
Szymon Jakubiak <szymon.jakubiak@inetum.world>
2021-04-07 17:29:30 +0200

    Dodany_sparametryzowany_skrypt2

---
+++ skrypt2_param.ps1
@@ -0,0 +1,13 @@
+﻿Param ([string]$Nazwa_aplikacji1, [string]$Nazwa_aplikacji2, [string]$Nazwa_aplikacji3,[string]$Nazwa_aplikacji4,[string]$Nazwa_aplikacji5)
+
+Install-PackageProvider -Chocolatey
+Install-PackageProvider -name ChocolateyGet -force -verbose
+
+Install-Package -provider ChocolateyGet -name $Nazwa_aplikacji1 -force -forcebootstrap -verbose
+Install-Package -provider ChocolateyGet -name $Nazwa_aplikacji2 -force -forcebootstrap -verbose 
+Install-Package -provider ChocolateyGet -name $Nazwa_aplikacji3 -force -forcebootstrap -verbose
+Install-Package -provider ChocolateyGet -name $Nazwa_aplikacji4 -force -forcebootstrap -verbose 
+Install-Package -provider ChocolateyGet -name $Nazwa_aplikacji5 -force -forcebootstrap -verbose 
+
+
+
