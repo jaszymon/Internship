6ce0134724b7ac8d23f87dbf6cfba0974e08ed1a
Szymon Jakubiak <szymon.jakubiak@inetum.world>
2021-04-07 16:53:57 +0200

    Dodany skrypt 1 i 4

---
+++ skrypt4.ps1
@@ -0,0 +1,11 @@
+﻿Install-PackageProvider -Chocolatey
+Install-PackageProvider -name ChocolateyGet -force -verbose
+
+Install-Package -provider ChocolateyGet -name GoogleChrome -force -forcebootstrap -verbose
+Install-Package -provider ChocolateyGet -name javaruntime -force -forcebootstrap -verbose 
+Install-Package -provider ChocolateyGet -name 7zip -force -forcebootstrap -verbose
+Install-Package -provider ChocolateyGet -name dotnet-5.0-sdk -force -forcebootstrap -verbose 
+Install-Package -provider ChocolateyGet -name python -force -forcebootstrap -verbose 
+
+
+
