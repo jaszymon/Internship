6d19b5586d00f9d19cb9a5be2f60a9ec26a63a47
Szymon Jakubiak <szymon.jakubiak@inetum.world>
2021-04-07 17:17:25 +0200

    Poprawki

--- skrypt4.ps1
+++ skrypt4.ps1
@@ -1,11 +1,16 @@
-﻿Install-PackageProvider -Chocolatey
-Install-PackageProvider -name ChocolateyGet -force -verbose
-
-Install-Package -provider ChocolateyGet -name GoogleChrome -force -forcebootstrap -verbose
-Install-Package -provider ChocolateyGet -name javaruntime -force -forcebootstrap -verbose 
-Install-Package -provider ChocolateyGet -name 7zip -force -forcebootstrap -verbose
-Install-Package -provider ChocolateyGet -name dotnet-5.0-sdk -force -forcebootstrap -verbose 
-Install-Package -provider ChocolateyGet -name python -force -forcebootstrap -verbose 
+﻿#Create script that will gather system information about host (CPU, RAM, Disks sizes, MAC Addresses, Name, Serial Number)
 
 
 
+$CPU = Get-WmiObject Win32_Processor | Select -ExpandProperty Name | Out-String
+$HDD = Get-Disk | Select -ExpandProperty FriendlyName | Out-String
+$RAM = (systeminfo | Select-String 'Total Physical Memory:').ToString().Split(':')[1].Trim()
+$MAC = get-netadapter | Select -ExcludeProperty MacAddres, Name | Select Name, MacAddress, Status | Out-String
+$Name = hostname
+$NumerSeryjny = (Get-WmiObject win32_bios | select Serialnumber).Serialnumber
+write-host 'Procesor: ' $CPU
+write-host 'Ilosc RAMu: ' $RAM
+write-host 'Dysk: ' $HDD 
+write-host 'MAC: ' $MAC
+write-host "Nazwa: " $Name
+write-host "Numer seryjny: " $NumerSeryjny
